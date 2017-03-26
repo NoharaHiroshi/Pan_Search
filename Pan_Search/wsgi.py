1baf46d8e37d4381f03cc20f0e1f79105577f321
Lands <380788433@qq.com>
2017-03-26 17:01:10 +0800

    更换版本

--- Pan_Search/wsgi.py
+++ Pan_Search/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Desktop project.
+WSGI config for Pan_Search project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Desktop.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Pan_Search.settings")
 
 application = get_wsgi_application()
