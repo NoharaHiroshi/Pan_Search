e3f552485c5afec46217b1c4a79a7cd5e052ff09
Lands <380788433@qq.com>
2017-05-02 21:59:24 +0800

    模型修改、ID生成

--- Pan_Search/settings.py
+++ Pan_Search/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'Pan_Search.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pan_search',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '',
+        'PORT': '3306',
     }
 }
 
--- search/models.py
+++ search/models.py
@@ -1,14 +1,24 @@
-from django.db import models
+# coding=utf-8
 
-# Create your models here.
+from django.db import models
+from lib.id_generate import id_generate
 
 
 class SearchResult(models.Model):
 
+    # 百度网盘、磁力链接
     TYPE_BAIDU, TYPE_MAGNET = range(2)
+    # 生效、已失效
+    STATUS_NORMAL, STATUS_EXPIRE, STATUS_DELETE = range(3)
+    # 文件类型
+    FILE_TYPE_COMMON, FILE_TYPE_RAR, FILE_TYPE_FILE, FILE_TYPE_IMG, \
+        FILE_TYPE_MUSIC, FILE_TYPE_VIDEO, FILE_TYPE_TXT = range(7)
 
-    STATUS_USED, STATUS_UNUSED = range(2)
+    id = models.BigIntegerField(max_length=32, primary_key=True, db_index=True, default=id_generate())
+    name = models.CharField(max_length=225,  db_index=True)
+    type = models.IntegerField(db_index=True, default=TYPE_BAIDU)
+    status = models.IntegerField(db_index=True, default=STATUS_NORMAL)
+    file_type = models.IntegerField(db_index=True, default=FILE_TYPE_COMMON)
 
-    name = models.CharField(max_length=50)
-    status = models.IntegerField(default=STATUS_USED)
-    type = models.IntegerField()
+
+
