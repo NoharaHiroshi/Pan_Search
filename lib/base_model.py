2207847960348046bbfcc5e0fb7bca08457abf96
Lands <380788433@qq.com>
2017-05-08 18:27:33 +0800

    添加自定义字段

--- lib/base_model.py
+++ lib/base_model.py
@@ -1,10 +1,25 @@
 # coding=utf-8
 
 from django.db import models
+import json
 
 
-class JSONEncodedDictField(models.CharField):
+class JSONEncodedDictField(models.TextField):
 
     def __init__(self, *args, **kwargs):
-        kwargs['dafault'] = dict()
+        self.content = dict()
         super(JSONEncodedDictField, self).__init__(*args, **kwargs)
+
+    def get_prep_value(self, value):
+        if value is not None:
+            self.content.update(json.dumps(value))
+            return self.content
+        else:
+            return value
+
+    def from_db_value(self, value):
+        if value is not None:
+            self.content = json.loads(value)
+            return self.content
+        else:
+            return self.content
