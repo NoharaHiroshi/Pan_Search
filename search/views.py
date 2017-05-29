dd83711c7343958b58de4029cdc3b6fbb2c0ec9f
Lands <380788433@qq.com>
2017-05-29 20:19:38 +0800

    前端开发

--- search/views.py
+++ search/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+import json
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+        'response': 'ok',
+        'info': ''
+    }
+    try:
+        return render(request, 'index.html', context)
+    except Exception as e:
+        print e
