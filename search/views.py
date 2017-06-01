1d56d0c01d2f4b339c6a72af76cc602742533577
Lands <380788433@qq.com>
2017-06-01 18:05:07 +0800

    post搜索结果

--- search/views.py
+++ search/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 
 import json
 from django.shortcuts import render
+from django.http import HttpResponse
+from search.models import SearchResult
 
 
 def index(request):
@@ -13,5 +15,33 @@ def index(request):
     }
     try:
         return render(request, 'index.html', context)
+    except Exception as e:
+        print e
+
+
+def search(request):
+    result = {
+        'response': 'ok',
+        'info': '',
+        'search_result_list': list()
+    }
+    try:
+        if request.method == 'POST':
+            search_info = request.POST.get('search', None)
+            all_query = SearchResult.objects.filter(
+                name=search_info
+            )
+            if all_query:
+                search_result_list = list
+                for query in all_query:
+                    search_dict = query.to_ict()
+                    search_result_list.append(search_dict)
+                result['search_result_list'] = search_result_list
+            else:
+                result['info'] = 'no result get'
+            context = json.dumps(result)
+            return HttpResponse(context, content_type="application/json")
+        else:
+            return 'this is a POST method'
     except Exception as e:
         print e
